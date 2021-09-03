73a858bbcb422dc230e1a395151928bfe5a89029
NetLSS <futureprogrammer@naver.com>
2021-09-03 20:42:32 +0900

    Feat: 알림 채널 구성하고 설정하기

--- app/src/main/java/com/lilcode/example/directreplynotifydemo/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/directreplynotifydemo/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.lilcode.example.directreplynotifydemo
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.lilcode.example.directreplynotifydemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,9 +15,31 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = requireNotNull(_binding)
 
+    private var notificationManager: NotificationManager? = null
+    private val channelID = "com.lilcode.example.directreplynotifydemo.news"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+
+        createNotificationChannel(channelID, "DirectReply News", "Example News Channel")
+    }
+
+    private fun createNotificationChannel(id: String, name: String, description: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            NotificationChannel(id, name, importance).apply {
+                this.description = description
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+                notificationManager?.createNotificationChannel(this)
+            }
+        }
+
     }
 }
