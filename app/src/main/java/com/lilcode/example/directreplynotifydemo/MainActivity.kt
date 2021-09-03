52df8c0723e5e82ef87ccb56996086491fa4d142
NetLSS <futureprogrammer@naver.com>
2021-09-03 21:23:38 +0900

    Feat: 응답 액션 생성하기

--- app/src/main/java/com/lilcode/example/directreplynotifydemo/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/directreplynotifydemo/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.lilcode.example.directreplynotifydemo
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.RemoteInput
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.drawable.Icon
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.lilcode.example.directreplynotifydemo.databinding.ActivityMainBinding
+import androidx.core.content.ContextCompat
+import android.app.RemoteInput
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,12 +53,26 @@ class MainActivity : AppCompatActivity() {
     fun sendNotification(view: View) {
         val replyLabel = "Enter your reply hehe"
         // key 와 label 로 RemoteInput 객체 생성
-        val remoteInput = RemoteInput.Builder(KEY_TEXT_REPLY)
+        val remoteInput = androidx.core.app.RemoteInput.Builder(KEY_TEXT_REPLY)
             .setLabel(replyLabel)
             .build()
 
+        // 팬딩 인텐트 생성하기
         val resultIntent = Intent(this, MainActivity::class.java)
-
         val resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val replyAction = NotificationCompat.Action.Builder(android.R.drawable.ic_dialog_info, "Reply", resultPendingIntent)
+            .addRemoteInput(remoteInput) // remote input 추가
+            .build()
+
+        val newMessageNotification = NotificationCompat.Builder(this, channelID)
+            .setColor(ContextCompat.getColor(this, R.color.design_default_color_primary))
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle("My Notification")
+            .setContentText("This is a test message")
+            .addAction(replyAction)
+            .build()
+
+        notificationManager?.notify(notificationId, newMessageNotification)
     }
 }
